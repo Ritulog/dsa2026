6d3712c072cab1ad75f9acba9a7989b8760e9df2
RituLog <ritu.k.gupta67@gmail.com>
2026-01-03 18:41:35 +0530

    "03/01/2026"

--- array/com/easy/RemoveDuplicate.java
+++ array/com/easy/RemoveDuplicate.java
@@ -21,6 +21,40 @@ public class RemoveDuplicate {
         return result;
     }
 
+    //2. approch
+    public int removeEle(int[] arr){
+        HashSet s = new HashSet<>();
+
+        int idx = 0;
+        for(int i=0; i< arr.length; i++){
+            if(!s.contains(arr[i])){
+                s.add(arr[i]);
+                arr[idx++] = arr[i];
+            }
+        }
+        return idx;
+    }
+
+    //3.approch
+    public int remove3(int[] arr){
+        int n = arr.length;
+        if(n<=1){
+            return n;
+        }
+
+        // start form second element
+        int idx = 1;
+        for(int i=1; i<n; i++){
+            if(arr[i] != arr[i-1]){
+                arr[idx++] = arr[i];
+            }
+        }
+        return  idx;
+    }
+
+
+
+
 
     public static void main(String[] args) {
         int arr[] = {2, 2, 2, 2, 2};
@@ -28,5 +62,19 @@ public class RemoveDuplicate {
         int arr2[] = {1, 2, 3};
         RemoveDuplicate r = new RemoveDuplicate();
         System.out.println(r.remove(arr1));
+
+        int result1 = r.remove3(arr1);
+        for(int i=0; i<result1; i++){
+            System.out.print(arr1[i] + " ");
+        }
+
+        System.out.println();
+
+        int result = r.removeEle(arr1);
+        for(int i=0; i<result; i++){
+            System.out.print(arr1[i] + " ");
+        }
+
+        System.out.println();
     }
 }
