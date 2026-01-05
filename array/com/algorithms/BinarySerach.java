21b1a24ccf00ad6ce46ef232eba110fd470dd789
RituLog <ritu.k.gupta67@gmail.com>
2026-01-05 22:25:27 +0530

    "05/01/2026"

---
+++ array/com/algorithms/BinarySerach.java
@@ -0,0 +1,35 @@
+package array.com.algorithms;
+
+public class BinarySerach {
+     public int binarySearch(int[] arr, int x){
+         int low=0, high = arr.length-1;
+         while(low <= high){
+             int mid = low +(high-low)/2;
+
+             if(arr[mid] == x){
+                 return mid;
+             } else if (arr[mid] < x) {
+                 low = mid+1;
+             }
+             else{
+                 high = mid-1;
+             }
+         }
+         return -1;
+     }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int x = 10;
+        BinarySerach s = new BinarySerach();
+        int result = s.binarySearch(arr, x);
+        if (result == -1) {
+            System.out.println("Element is not present in array");
+        }
+        else {
+            System.out.println("Element is present at " + "index " + result);
+        }
+
+    }
+
+}
