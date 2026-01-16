b83c8bb65c61299011908159952248123e415c14
RituLog <ritu.k.gupta67@gmail.com>
2026-01-16 21:53:03 +0530

    "16/01/2026"

--- array/com/easy/MaxSumSubArray.java
+++ array/com/easy/MaxSumSubArray.java
@@ -25,9 +25,23 @@ public class MaxSumSubArray {
 
     }
 
+    // 2nd method
+    public static int maxSubArraySum(int[] arr) {
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+
     public static void main(String[] args) {
       int[] a = {4,-2,-3,4,-1,-2,1,5,-3};
       System.out.println(UsingKadanesAlgorithm(a));
+        System.out.println(maxSubArraySum(a));
 
     }
 }
