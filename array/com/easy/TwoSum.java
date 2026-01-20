5468842b1c87c1aaf978dca97a68f5718a04ccee
RituLog <ritu.k.gupta67@gmail.com>
2026-01-20 22:45:22 +0530

    "20/01/2026"

---
+++ array/com/easy/TwoSum.java
@@ -0,0 +1,64 @@
+package array.com.easy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    // naive approch
+    static boolean twoSum(int[] arr, int target){
+        for(int i=0; i< arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //better approch
+    static boolean binarySearch(int[] arr, int left,
+                                int right, int target){
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return true;
+            if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+
+
+    static boolean twoSum1(int[] arr, int target){
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            // Use binary search to find the complement
+            if (binarySearch(arr, i + 1, arr.length - 1,
+                    complement))
+                return true;
+        }
+        // If no pair is found
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] arr = { 0, -1, 2, -3, 1 };
+        int target = -2;
+
+        if (twoSum(arr, target))
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
