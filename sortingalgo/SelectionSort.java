4ed5f785114fd7424e5858ab8172e2a86441f871
RituLog <ritu.k.gupta67@gmail.com>
2026-01-22 22:30:16 +0530

    "selection sort"

---
+++ sortingalgo/SelectionSort.java
@@ -0,0 +1,34 @@
+package sortingalgo;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // find minimum element in unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
