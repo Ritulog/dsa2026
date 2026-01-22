4eb8edcfd5749e52d2562ae8d9a3ed600e64b0aa
RituLog <ritu.k.gupta67@gmail.com>
2026-01-22 21:29:54 +0530

    "22/01/2026"

--- array/com/easy/TwoSumCloseTo0.java
+++ array/com/easy/TwoSumCloseTo0.java
@@ -1,5 +1,7 @@
 package array.com.easy;
 
+import java.util.Arrays;
+
 public class TwoSumCloseTo0 {
     static int minAbsSumPair(int[] arr) {
 
@@ -23,6 +25,51 @@ public class TwoSumCloseTo0 {
         }
         return res;
     }
+
+
+    // better approch
+    static int minAbsPair(int[] arr) {
+
+            Arrays.sort(arr);
+
+            int n = arr.length;
+
+            int res = Integer.MAX_VALUE;
+
+            for (int i = 0; i < n; i++) {
+
+                int x = arr[i];
+
+                int left = i + 1, right = n - 1;
+
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+
+                    int curr = arr[mid] + x;
+
+                    // If exact pair is found
+                    if (curr == 0) {
+                        return 0;
+                    }
+
+                    if (Math.abs(curr) < Math.abs(res)) {
+                        res = curr;
+                    }
+
+                    else if (Math.abs(curr) == Math.abs(res)) {
+                        res = Math.max(res, curr);
+                    }
+
+                    if (curr < 0) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return res;
+        }
 
 
     public static void main(String[] args) {
